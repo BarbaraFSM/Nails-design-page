b14f7c8d080671e18b7f671d5783c8575df8988c
Barbara Ferreira <153443016+BarbaraFSM@users.noreply.github.com>
2024-12-03 02:54:17 -0300

    Create header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Manicure Profissional</h1>
+      <p>Cuidando de você com estilo e qualidade!</p>
+    </header>
+  );
+};
+
+export default Header;
