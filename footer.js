8c6f3bea0a379d4fafc10553c9b5049f3ffaeb67
Barbara Ferreira <153443016+BarbaraFSM@users.noreply.github.com>
2024-12-03 02:58:22 -0300

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 Manicure Profissional. Todos os direitos reservados.</p>
+      <p>Contato: (21) 99999-9999</p>
+    </footer>
+  );
+};
+
+export default Footer;
